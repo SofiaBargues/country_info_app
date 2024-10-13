613c19074f2b004bbdc63bdfcdbcd4188016e1d1
Sofia Bargues <barguessofia@gmail.com>
2024-10-13 15:31:59 +0100

    buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,50 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+
+  const [countries, setCountries] = useState([
+    { countryCode: "AD", name: "Andorra" },
+    { countryCode: "AL", name: "Albania" },
+  ]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("/api/v1/countries", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          console.log(response);
+          const result = await response.json();
+          setCountries(result);
+        } else {
+          const errorData = await response.json();
+          console.log(errorData);
+          throw new Error(errorData.message || "Error en la solicitud");
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+  return (
+    <div>
+      {countries.map((x, i) => (
+        <button className="border m-2 rounded-md grid " key={i}>
+          {x.name}
+        </button>
+      ))}
+    </div>
+  );
 }
